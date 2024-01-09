e966c9e99229babce9ce5d252add4b8c22218bde
Firuzakerimova <bd7mbuwz1@code.edu.az>
2024-01-09 01:08:15 -0800

    colorlib

---
+++ src/components/Header.jsx
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const header = () => {
+    return (
+        <div>
+            <div className='header'>
+                <h1>Consulting</h1>
+                <ul >
+                    <li>HOME</li>
+                    <li>ABOUT</li>
+                    <li>SERVICES</li>
+                    <li>BLOG</li>
+                    <li>CONTACT</li>
+                </ul>
+            </div>
+            <div className='sec-1'>
+                <div>
+                    <div className='section'>
+                        <h3>COMMITTED TO SECCESS</h3>
+                        <h1>We help to grow your business</h1>
+                        <p>Mollit anim laborum.Dvcuis aute serunt iruxvfg dhjkolohr indd re voluptate</p></div>
+                </div>
+            </div>
+            <div className='sec-3'>
+                <div className='section-3'>
+                   <div>
+                   <h3>OUR TOP SERVICES</h3>
+                    <h1>Our Best Services</h1>
+                    <p>Mollit anim laborum duis adseu dolor iuyn voluptcate velit ess cillum dolore egru lofrre dsu quality mollit anim laborumuis au dolor in voluptate velit cillu.
+                    </p>
+                    <p>Mollit anim laborum.Dvcuis aute serunt iruxvfg dhjkolohr indd re voluptate velit esscillumlore eu quife nrulla parihatur. Excghcepteur sfwsignjnt occa cupidatat non aute iruxvfg dhjinulpadeserunt moll.</p>
+                    <div>
+                        <a href="">MORE ABOUT US</a>
+                    </div>
+                   </div>
+                </div>
+
+            </div>
+            <div className='sec-4'>
+
+            </div>
+        </div>
+    )
+}
+
+export default header
